98f4193fba38d7fd15f996fe22b248bc56ed8e54
GheistLycis <bruno.nitro48@gmail.com>
2022-04-01 12:20:05 -0300

    general structure

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+app.set("view engine", "ejs")
+
+app.set("views", path.join(__dirname, "views"))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({extended: true}))
+
+
+//ROUTES
+app.get("/", (req, res)=>{
+    res.render("home", {
+        title: "Home"
+    })
+})
+app.use((req, res)=>{
+    res.send("Page not found... =/")
+})
+
+
+//SERVER
+const port = process.env.PORT || 8080
+app.listen(port, ()=>{console.log(`Server on! Port: ${port}`)})
